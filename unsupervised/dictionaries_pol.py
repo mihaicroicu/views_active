57a62bcaf5a1b670900e00c235da200cef24c7d3
Mihai Croicu <mihai.croicu@pcr.uu.se>
2025-03-18 16:14:46 +0100

    Unsupervised labeler (ipynb)
    Using Croicu(2024)

---
+++ unsupervised/dictionaries_pol.py
@@ -0,0 +1,15 @@
+__author__ = 'mihaicroicu'
+#All the WN hyponyms of place_of_worship.n.01 (single) synset:
+
+#religious_buildings = set(['bethel', 'synagogue', 'ashram', 'altar', 'mosque', 'basilica', 'mosk', 'pagoda', 'kirk', 'shrine', 'masjid', 'abbey', 'stupa', 'church', 'oracle', 'cathedral', 'chapel', 'chantry', 'temple', 'conventicle'])
+political_actor = set(['incumbent', 'elect', 'candidate', 'elector', 'vote', 'incumbent', 'voter', 'gerrymandering', 'constituency', 'ballot', 'referendum', 'franchise', 'franchize', 'canvass', 'caucus', 'plebiscite', 'runoff', 'run-off', 'enfranchise', 'disenfranchise'])
+
+#All the WN hyponyms of religious_person.n.01 and religion.n.02
+
+political_actor2 = set(['liberal', 'anarchist', 'communist', 'wing', 'right-wing', 'left-wing'])
+#All the WN hyponyms of attack.v.09:
+
+political_seconds = set(['party', 'protest'])
+
+#All the WN hyponyms of attack.v.09:
+#All the WM hyponyms of kill.v.01:
---
+++ unsupervised/dictionaries_rel.py
@@ -0,0 +1,20 @@
+__author__ = 'mihaicroicu'
+#All the WN hyponyms of place_of_worship.n.01 (single) synset:
+
+religious_buildings = set(['bethel', 'synagogue', 'ashram', 'altar', 'mosque', 'basilica', 'mosk', 'pagoda', 'kirk', 'shrine', 'masjid', 'abbey', 'stupa', 'church', 'oracle', 'cathedral', 'chapel', 'chantry', 'temple', 'conventicle'])
+
+
+#All the WN hyponyms of religious_person.n.01 and religion.n.02
+
+religious_terms = set(['shiah', 'shiite', 'vicar', 'hinayana', 'rabbi', 'shia', 'agnostic', 'evangelist', 'preacher', 'jihad', 'alawite', 'prayer', 'moonie', 'religious', 'bishop', 'jihadist', 'mozaism', 'twelver', 'catholic', 'taoism', 'guru', 'sharia', 'moslem', 'hindu', 'buddhist', 'ayatollah', 'religion', 'buddhism', 'cardinal', 'shinto', 'hinduism', 'sunnite', 'brahmanism', 'imam', 'rastafari', 'sufi', 'christian', 'exarch', 'orthodox', 'monotheist', 'zen', 'muslim', 'hindoo', 'hindu', 'taoist', 'voodoo', 'lama', 'copt', 'coptic', 'moslem', 'shaman', 'mujtihad', 'shamanism', 'mullah', 'sunni', 'missionary', 'adventist', 'nun', 'mahayana', 'judaism', 'sheyria', 'mujahid', 'prophet', 'deobandi', 'churchgoer', 'caliph', 'believer', 'mandaean', 'deacon', 'priest', 'sect', 'clergy', 'nazarene', 'tao', 'eparch', 'preach'])
+
+#All the WN hyponyms of attack.v.09:
+
+attack_terms = set(['blow', 'demolish', 'duel', 'engage', 'alight', 'scuffle', 'fight', 'skirmish', 'attack', 'battle', 'war'])
+
+#All the WM hyponyms of kill.v.01:
+
+kill_terms = set(['exterminate', 'dispatch', 'erase', 'poison', 'strangle', 'martyr', 'zap', 'dismember', 'neutralize', 'butcher', 'eliminate', 'destroy', 'assassinate', 'electrocute', 'murder', 'sacrifice', 'massacre', 'stone', 'drown', 'lynch', 'decapitate', 'shoot', 'execute', 'decimate', 'impale'])
+
+
+
